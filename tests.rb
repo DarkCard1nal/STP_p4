4dddb79d9f017bd98e0a3ff163ada6ab7b081f19
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-10-07 10:09:59 +0300

    Added VerifierIPv4 class to verify IPv4 addresses, added tests 	modified:   tests.rb 	modified:   verifier_ipv4.rb

--- tests.rb
+++ tests.rb
@@ -4,18 +4,29 @@ require_relative 'verifier_ipv4'
 
 puts('Start tests!')
 
-# rubocop:disable Layout/ArrayAlignment, Layout/SpaceInsideArrayLiteralBrackets
-arr = [,]
-# rubocop:enable Layout/ArrayAlignment, Layout/SpaceInsideArrayLiteralBrackets
+# rubocop:disable Layout/SpaceInsideArrayLiteralBrackets, Layout/ArrayAlignment
+arr = [	['192.168.1.1', true],
+		['	1.205.200.0 ', true],
+		['256.0.0.1', false],
+		['10.4.5.-1', false],
+		['10.1 4.6.1', false],
+		['10.1	4.7.1', false],
+		['10.5.4.', false],
+		['10.00.3.1', false],
+		['1', false],
+		['0.0.0.0', true],
+		['0.0.0', false],
+		['1.8v6.1.1', false]]
+# rubocop:enable Layout/SpaceInsideArrayLiteralBrackets, Layout/ArrayAlignment
 
 completed = 0
 arr.each do |test|
 	p test[0]
-	result = (test[0])
+	result = VerifierIPv4.verify(test[0])
 	p result
 	puts("Result: #{result == test[1]}")
 	completed += result == test[1] ? 1 : 0
 end
 
-puts("\nPassed #{completed}/#{arr.length} tests!!")
+puts("\nPassed #{completed}/#{arr.size} tests!!")
 puts('Stop tests!')
--- verifier_ipv4.rb
+++ verifier_ipv4.rb
@@ -1,7 +1,38 @@
 # frozen_string_literal: true
 
-require_relative 'constants'
-
-# Class
+# Static class that checks the canonical IPv4 address for correctness
 class VerifierIPv4
+	def initialize
+		raise NotImplementedError
+	end
+
+	# Static method checks octet for correctness, returns true
+	def self.verify(address)
+		address = address.strip.split('.')
+
+		return false if address.size != 4
+
+		address.map! do |octet|
+			verify_octet(octet)
+		end
+
+		return true if address.count(true) == 4
+
+		false
+	end
+
+	# Static method checks octet for correctness, returns true
+	def self.verify_octet(octet)
+		return false if octet.length > 3 || octet.empty?
+
+		return false if octet =~ /\A0[0-9]+/ || octet =~ /\s/
+
+		return false if octet !~ /\A\d+\z/
+
+		num = octet.to_i
+
+		num >= 0 && num < 256
+	end
+
+	private_class_method :verify_octet
 end
